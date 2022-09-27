1ec9b69eeb8a97706f6db1d906d4047e8cbf5831
devock <ockjungbong@gmail.com>
2022-09-27 23:59:21 +0900

    initial setting

---
+++ src/test/java/com/example/junitProject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
