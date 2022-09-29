84a08beb490a1a21c266a3f9c5d2e3dab246cf06
devock <ockjungbong@gmail.com>
2022-09-30 00:29:11 +0900

    Repository 테스트 환경 셋팅 완료

--- src/main/java/com/example/junitProject/domain/BookRepository.java
+++ src/main/java/com/example/junitProject/domain/BookRepository.java
@@ -1,5 +1,7 @@
 package com.example.junitProject.domain;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
--- src/main/java/com/example/junitProject/service/BookService.java
+++ src/main/java/com/example/junitProject/service/BookService.java
@@ -1,5 +1,8 @@
 package com.example.junitProject.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
 }
